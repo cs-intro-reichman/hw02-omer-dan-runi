6116c78145def0506d7f68350d3e0652bcf7f1b7
omer-dan-runi <omer.dan01@post.runi.ac.il>
2023-12-25 13:48:56 +0200

    DamkaBoard necessary only for the test

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -28,6 +28,13 @@ public class DamkaBoard {
 			// Resetting j
 			j = 0;
 
+			// This is unnecessary, its just so the test will pass
+			if (i % 2 != 0)
+			{
+				// Adding a " " as the last char.
+				System.out.print(" ");
+			}
+
 			System.out.println();
 			i++;
 		}
